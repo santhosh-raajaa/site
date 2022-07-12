daf7db9d4effede3c20aed079b92043338ab6519
santhosh-raajaa <santhoshraajaa611@gmail.com>
2022-07-12 19:11:52 +0530

    added_typing_effect

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,14 +3,22 @@ import './header.css'
 import CTA from './CTA'
 import Me from '../../assets/2.jpg'
 import HeaderSocials from './HeaderSocials'
+import {useTypewriter,Cursor } from 'react-simple-typewriter'
 
 const Header = () => {
+
+  const {text} = useTypewriter({
+    words:['an Aeronautic Engineer','a Drone Designer','a Developer','a 3D Artist','a Product Designer'],
+    loop:0,
+    delaySpeed:1000,
+  })
+
   return (
     <header>
       <div className="container header__container">
         <h4>Hey there! I'm</h4>
         <h1>Santhosh Raajaa</h1>
-        <h4 className='text-light'>Developer</h4>
+        <h4 className='text-light'>I'm {text} <Cursor/> </h4>
 
         <CTA/>
         <HeaderSocials/>
